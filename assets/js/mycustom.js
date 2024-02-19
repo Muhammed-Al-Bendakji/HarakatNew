ae4163cd56b83673f31fba5be30d6c668641d81d
Muhammed-Al-Bendakji <mohammed.al.bendkji@gmail.com>
2024-02-19 04:23:15 +0300

    majore change

---
+++ assets/js/mycustom.js
@@ -0,0 +1,28 @@
+// window.addEventListener('load', function(){
+//     var newVideo = document.getElementById("videoId");
+//     newVideo.addEventListener('ended', function() {
+//         console.log('ss');
+
+//         this.currentTime = 0;
+//         this.play();
+//     }, false);
+
+//     newVideo.play();
+
+// });
+
+
+// document.getElementById('videoId')[0].onended = function () {
+// console.log('ss');
+
+//     this.load();
+//     this.play();
+//   };
+
+// var video = document.querySelector("video");
+
+// video.onended = (event) => {
+//     console.log(
+//       "Video stopped either because it has finished playing or no further data is available.",
+//     );
+// };
